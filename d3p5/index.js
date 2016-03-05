f2c634168a4ef286299421c8f68d21d80466dc12
meng <pingf0@gmail.com>
2016-03-05 11:19:40 +0800

    add react tutorial 6&7

--- d3p5/index.js
+++ d3p5/index.js
@@ -2,5 +2,6 @@
 //require('./webpack_components/test/simple')
 //require('./webpack_components/test/tutorial1')
 //require('./webpack_components/test/tutorial2')
-require('./webpack_components/test/tutorial3')
+//require('./webpack_components/test/tutorial3')
+require('./webpack_components/test/tutorial6and7')
 
---
+++ d3p5/webpack_components/test/tutorial6and7.js
@@ -0,0 +1,60 @@
+// tutorial6.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+var marked = require('marked')
+
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+        return { __html: rawMarkup };
+    },
+
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
+                <Comment author="Jesse MENG">This is __another__ comment</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
