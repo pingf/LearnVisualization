e1ff017672d26f8f433d4f59703353440f70072f
meng <pingf0@gmail.com>
2016-03-05 11:27:09 +0800

    add react tutorial 8 9 10

--- d3p5/index.js
+++ d3p5/index.js
@@ -3,5 +3,6 @@
 //require('./webpack_components/test/tutorial1')
 //require('./webpack_components/test/tutorial2')
 //require('./webpack_components/test/tutorial3')
-require('./webpack_components/test/tutorial6and7')
+//require('./webpack_components/test/tutorial6and7')
+require('./webpack_components/test/tutorial8910')
 
