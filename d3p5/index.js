0384e051dfc1804feeed8705278ad449b5e5da09
meng <pingf0@gmail.com>
2016-03-19 00:39:58 +0800

    refactor project structure

--- d3p5/index.js
+++
@@ -1,8 +0,0 @@
-//require('./p5examples/charts')
-//require('./webpack_components/test/simple')
-//require('./webpack_components/test/tutorial1')
-//require('./webpack_components/test/tutorial2')
-//require('./webpack_components/test/tutorial3')
-//require('./webpack_components/test/tutorial6and7')
-require('./webpack_components/test/tutorial11121314')
-
---
+++ index.js
@@ -0,0 +1,8 @@
+//require('./react_learn/p5examples/charts')
+//require('./react_learn/webpack_components/test/simple')
+//require('./react_learn/webpack_components/test/tutorial1')
+//require('./react_learn/webpack_components/test/tutorial2')
+//require('./react_learn/webpack_components/test/tutorial3')
+//require('./react_learn/webpack_components/test/tutorial6and7')
+require('./react_learn/webpack_components/test/tutorial11121314')
+
